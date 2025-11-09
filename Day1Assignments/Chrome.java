5c0140b8ab32f2b94819d72cc225352fae69c6f8
Lakshmi S <lakshmisuresheee@gmail.com>
2025-11-09 13:08:34 +0530

    Add files via upload
    Day1

---
+++ Day1Assignments/Chrome.java
@@ -0,0 +1,21 @@
+package Day1Assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 float version= 91.0f;
+		 String developer = "Google";
+		 boolean isBeta = false;
+		 int releaseYear = 2008;
+		 char shortcutKey = 'C';
+		 
+		System.out.println("Chrome version : " + version);
+		System.out.println("Chrome developer : " + developer);
+		System.out.println("Chrome isBeta : " + isBeta);
+		System.out.println("Chrome releaseYear : " + releaseYear);
+		System.out.println("Chrome shortcutKey : " + shortcutKey);
+	
+	}
+
+}
---
+++ Day1Assignments/FindOddNumbers.java
@@ -0,0 +1,20 @@
+package Day1Assignments;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The odd numbers between 1 to 10");
+		int maxRange = 10;
+
+		for (int i = 1; i <= maxRange; i++) {
+
+			if (i % 2 == 1) {
+
+				System.out.println(i);
+			}
+
+		}
+	}
+
+}
